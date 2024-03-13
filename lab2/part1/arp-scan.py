08fbadcae67b6ca952e09fe3c2b103707e6a5f7e
durandnico <nicod4956@gmail.com>
2024-03-13 02:40:58 +0100

    déplacement du code de arp-scan a network-util car plus logique

--- lab2/part1/arp-scan.py
+++ lab2/part1/arp-scan.py
@@ -1,49 +1,57 @@
 import socket
 from scapy.all import *
 import network_util as nutil
+from threading import Thread
 
-def arp_scan(ip_addrs):
+res = []
 
+def arp_scan(ipv4):
+    global res
+    print("scanning : " +  ipv4)
+
+    arp_request = ARP(pdst=ipv4)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+        
+    # send request
+    answerList = srp(arp_request_broadcast, timeout=1, verbose=False)
+    
+    # parse response
+    for answer in answerList[0]:
+        res.append(answer[1].psrc + " | " + answer[1].hwsrc)
+
+def arp_scan_ips(ip_addrs):
     for ip in ip_addrs:
-        print("Scanning " + ip)
+        thread = Thread(target = arp_scan, args = (ip,))
+        thread.start()
+    
+    thread.join()
+    for r in res:
+        print(r)
 
-        arp_request = scapy.ARP(pdst=ip)
-        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-        arp_request_broadcast = broadcast/arp_request
-        
-        # send request
-        answerList = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
-        
-        # parse response
-        for answer in answerList[0]:
-            print(answer[1].psrc + " " + answer[1].hwsrc)
+def arp_scan_network(subnet, mask):
+    mask_split = list(  map( int, mask.split('.') ))
+    subnet_split = list(map(int, subnet.split('.')))
+    
+    ip_addrs = []
+    for i in range(256 - mask_split[0]):
+        for j in range(256 - mask_split[1]):
+            for k in range(256 - mask_split[2]):
+                for l in range(256 - mask_split[3]):
+                    ip_addrs.append(str(subnet_split[0] + i) + "." + str(subnet_split[1] + j) + "." + str(subnet_split[2] + k) + "." + str(subnet_split[3] + l))
 
+
+    ip_addrs.pop() # remove broadcast address
+    ip_addrs.pop(0) # remove network address
+    arp_scan_ips(ip_addrs)
 
 
 def main():
-    iface = "wlp0s20f3"
+    iface = "eth0"
     ip = nutil.get_ip(iface)
     mask = nutil.get_mask(iface)
     subnet = nutil.get_subnet(ip, mask)
-    print(subnet)
-
-    mask_split = map( int, mask.split('.') )
-    subnet_split = map(int, subnet.split('.'))
-    
-    # calculate number of hosts
-    num_hosts = 1
-    for i in range(4):
-        num_hosts *= 256 - int(mask_split[i])
-    num_hosts -= 2 # subtract network and broadcast addresses
-
-
-    ip_addrs = []
-    for i in range(1, 255):
-        ip_addrs.append(subnet + "." + str(i))
-
-    ip_addrs.pop(0) # remove network address
-    ip_addrs.pop() # remove broadcast address
-    arp_scan(ip_addrs)
+    arp_scan_network(subnet, mask)
 
 if __name__ == "__main__":
     main()
--- lab2/part1/network_util.py
+++ lab2/part1/network_util.py
@@ -18,6 +18,23 @@ def get_subnet(ip, mask):
         subnet.append(str(int(ip[i]) & int(mask[i])))
     return '.'.join(subnet)
 
+
+def get_ips_in_subnet(subnet, mask):
+    mask_split = list(  map( int, mask.split('.') ))
+    subnet_split = list(map(int, subnet.split('.')))
+    
+    ip_addrs = []
+    for i in range(256 - mask_split[0]):
+        for j in range(256 - mask_split[1]):
+            for k in range(256 - mask_split[2]):
+                for l in range(256 - mask_split[3]):
+                    ip_addrs.append(str(subnet_split[0] + i) + "." + str(subnet_split[1] + j) + "." + str(subnet_split[2] + k) + "." + str(subnet_split[3] + l))
+
+    ip_addrs.pop() # remove broadcast address
+    ip_addrs.pop(0) # remove network address
+    return ip_addrs
+
+
 def main():
     iface = "wlp0s20f3"
     print (get_ip(iface))
